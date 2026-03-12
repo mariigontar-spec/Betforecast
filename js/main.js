64066e37c5e59f841052700927abed4216a8153f
mariigontar-spec <marii.gontar@gmail.com>
2026-03-12 16:58:28 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
+fetch("data/news.json")
+  .then(response => response.json())
+  .then(data => {
+    const newsList = document.getElementById("news-list");
 
+    data.forEach(article => {
+      const card = document.createElement("div");
+      card.className = "news-card";
+
+      card.innerHTML = `
+        <h3>${article.title}</h3>
+        <p>${article.text}</p>
+      `;
+
+      newsList.appendChild(card);
+    });
+  });
