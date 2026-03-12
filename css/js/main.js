6bb392b19daff529e765abfa8aa69c75c7a6415e
mariigontar-spec <marii.gontar@gmail.com>
2026-03-12 16:02:37 +0200

    Update main.js

--- css/js/main.js
+++ css/js/main.js
@@ -1,1 +1,20 @@
-console.log("BetForecast loaded");
+fetch("data/news.json")
+  .then(response => response.json())
+  .then(data => {
+
+    const newsContainer = document.getElementById("news");
+
+    data.forEach(article => {
+
+      const div = document.createElement("div");
+
+      div.innerHTML = `
+        <h3>${article.title}</h3>
+        <p>${article.category} | ${article.date}</p>
+      `;
+
+      newsContainer.appendChild(div);
+
+    });
+
+  });
