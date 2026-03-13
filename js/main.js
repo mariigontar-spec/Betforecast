6d533f876a538d44cda1c3f22ad9f69c4f3bdc91
mariigontar-spec <marii.gontar@gmail.com>
2026-03-13 14:09:18 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,31 +1,43 @@
+// LOAD NEWS
+
 fetch("data/news.json")
   .then(response => response.json())
   .then(data => {
 
     const newsList = document.getElementById("news-list");
 
-    data.slice(0, 6).forEach(article => {
+    if (!newsList) return;
+
+    data.slice(0,6).forEach(article => {
 
       const card = document.createElement("div");
       card.className = "news-card";
 
-    card.innerHTML = `
-<img src="${article.image}">
-<h3><a href="#">${article.title}</a></h3>
-<p>${article.text}</p>
-`;
+      card.innerHTML = `
+        <img src="${article.image}">
+        <h3><a href="#">${article.title}</a></h3>
+        <p>${article.text}</p>
+      `;
 
       newsList.appendChild(card);
 
     });
 
-  });
+});
+
+
+// LOAD MATCHES
+
 fetch("data/matches.json")
   .then(response => response.json())
   .then(data => {
+
     const matchesList = document.getElementById("matches-list");
 
+    if (!matchesList) return;
+
     data.forEach(match => {
+
       const row = document.createElement("tr");
 
       row.innerHTML = `
@@ -35,5 +47,7 @@ fetch("data/matches.json")
       `;
 
       matchesList.appendChild(row);
+
     });
-  });
+
+});
