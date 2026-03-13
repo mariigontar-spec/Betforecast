fa2e065d2ece4af18ccf30a985977e0be9f10802
mariigontar-spec <marii.gontar@gmail.com>
2026-03-13 15:23:35 +0200

    Update article.js

--- js/article.js
+++ js/article.js
@@ -1,1 +1,18 @@
+const params = new URLSearchParams(window.location.search);
+const articleId = params.get("id");
 
+fetch("data/news.json")
+.then(response => response.json())
+.then(data => {
+
+const article = data.find(n => n.id == articleId);
+
+const container = document.getElementById("article");
+
+container.innerHTML = `
+<h2>${article.title}</h2>
+<img src="${article.image}" style="width:100%;max-width:700px">
+<p>${article.text}</p>
+`;
+
+});
