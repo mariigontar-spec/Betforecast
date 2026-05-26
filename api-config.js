ccc91ed41d4b432e5d69bb3f8cd88f5aa02c014d
mariigontar-spec <marii.gontar@gmail.com>
2026-05-26 15:08:14 +0300

    Update api-config.js

--- api-config.js
+++ api-config.js
@@ -1,9 +1,6 @@
+
 const BF_API = {
   key: "f9ec01280f74fd2a13eddee6cc9f2e4e",
   host: "v3.football.api-sports.io",
   baseUrl: "https://v3.football.api-sports.io"
-};
-const FD_API = {
-  key: "19bca927b9cc4bcaa099a6c0fa4722a2",
-  baseUrl: "https://api.football-data.org/v4"
 };
