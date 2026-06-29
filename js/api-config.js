a32e26d08d6b0b8f0d1eeadaf7d3f15a8368c683
mariigontar-spec <marii.gontar@gmail.com>
2026-06-29 12:43:59 +0300

    Update api-config.js

--- js/api-config.js
+++ js/api-config.js
@@ -8,7 +8,7 @@ window.BF_API = {
     Direct API-Football / API-SPORTS mode:
     Replace PASTE_YOUR_API_KEY_HERE with the real key.
   */
-  key: "PASTE_YOUR_API_KEY_HERE",
+  key: "f2eebf6963d937cdbd298021a9490e20",
   host: "v3.football.api-sports.io",
   baseUrl: "https://v3.football.api-sports.io",
 
