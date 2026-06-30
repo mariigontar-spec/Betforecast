3674483ca0833a34b18624e5b348ab02b6b2952b
mariigontar-spec <marii.gontar@gmail.com>
2026-06-30 12:14:36 +0300

    Update api-config.js

--- api-config.js
+++ api-config.js
@@ -1,7 +1,13 @@
 window.BF_API = {
   provider: "football-data",
-  key: "19bca927b9cc4bcaa099a6c0fa4722a2",
+
+  key: "f2eebf6963d937cdbd298021a9490e20",
+
   baseUrl: "https://api.football-data.org/v4",
+
   competition: "WC",
-  season: 2026
+
+  season: 2026,
+
+  timezone: "Europe/Tallinn"
 };
