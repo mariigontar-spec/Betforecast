25d613ea95d757f2f07eeddac9228d50a90c80a7
mariigontar-spec <marii.gontar@gmail.com>
2026-07-24 17:21:50 +0300

    Bump shared skin manager to v16

--- api-config.js
+++ api-config.js
@@ -23,7 +23,7 @@ window.BF_TIMEZONE = {
 
   const script = document.createElement("script");
   script.id = scriptId;
-  script.src = "/js/site-skin-manager.js?v=15";
+  script.src = "/js/site-skin-manager.js?v=16";
   script.defer = true;
   document.head.appendChild(script);
 })();
