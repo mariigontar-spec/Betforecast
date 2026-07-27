1ea832b2a6adabe25e321538f1cd59a665355f1d
mariigontar-spec <marii.gontar@gmail.com>
2026-07-27 18:03:01 +0300

    Load dynamic background manager v21

--- api-config.js
+++ api-config.js
@@ -23,7 +23,7 @@ window.BF_TIMEZONE = {
 
   const script = document.createElement("script");
   script.id = scriptId;
-  script.src = "/js/site-skin-manager.js?v=20";
+  script.src = "/js/site-skin-manager.js?v=21";
   script.defer = true;
   document.head.appendChild(script);
 })();
